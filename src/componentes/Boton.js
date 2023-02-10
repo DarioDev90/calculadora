2fb373fc36b942a80e6d505ed0006140a6253130
EderHdz <dariotecsoft90@gmail.com>
2023-02-10 16:21:06 -0500

    Final commit

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,11 +1,10 @@
-/* eslint-disable eqeqeq */
 import React from 'react';
 import '../hojas-de-estilo/Boton.css'
 
 function Boton(props) {
 
   const esOperador = valor => {
-    return isNaN(valor) && (valor != '.') && (valor != '=');
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
   };
   return (
     <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()} onClick={() => props.manejarClic(props.children)}>
