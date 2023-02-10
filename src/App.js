a2657f8e1e112bb5c53733ed4efbf1c4a59843ba
EderHdz <dariotecsoft90@gmail.com>
2023-02-09 21:04:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={freeCodeCampLogo}
+        className='freecodecamp-logo'
+         alt='Logo de freeCodeCamp' />
+      </div>
     </div>
   );
 }
